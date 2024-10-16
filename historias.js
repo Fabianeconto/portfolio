20838a34185d84d6db90c62280e113726051e386
Fabianeconto <163874473+Fabianeconto@users.noreply.github.com>
2024-10-16 16:33:21 -0300

    Add files via upload

---
+++ historias.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'sobre Biotecnologia',
+    'Trabalho em grupo, sobre as plantas',
+    'Fizemos um trabalho onde tinhamos que escolher um planta e cuidar dela até um determinado tempo, eu e meu grupo escolhemos a cebolinha, pois era mais fácil de cuidar'
+
+)
+
+criaCartao(
+    'Sobre o pensamento computacional',
+    'Durante a aula de pensamento computacional',
+    'Eu e minhas amigas estavamos na aula de pensamento computacional, já tinhamos feito todas as licões, logo decidimos tirar uma foto, pois tinhamos montado um grupo para nois, mas estava sem foto. '
+)
+
+criaCartao(
+    'Passeio',
+    'Passeio de Teatro',
+    'No 9° eu e minhas amigas fomos a um passeio, fomos assistir um teatro que parecia com a cinderela, eu e minhas amigas ficamos zoando durante a peça inteira. '
+
+)
+
+criaCartao(
+    'Projeto de Vida',
+    'Trabalho de Entrevista',
+    'No 1° ano do ensino médio, nossa professora deu um trabalho em grupo, onde tinhamos que entrevistar um empresário, eu e minhas amigas decidimos entrevistar meu pai, nós fizemos o trabalho quase um mês anets da entrega, pois a entrega seria depois das férias de Julho, mas nós já tinhamos terminado, onde acabamos surpreendendo a professora. '
+)
